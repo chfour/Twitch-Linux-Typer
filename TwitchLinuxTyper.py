956a0118746b61fa2c6f7d5be993e24f700c6b4b
chfour <michaleklitwin@gmail.com>
2022-01-23 18:03:36 +0100

    pyautogui.hotkey() fix

--- TwitchLinuxTyper.py
+++ TwitchLinuxTyper.py
@@ -9,8 +9,7 @@ while True:
     user, message = MyChat.listen()
     if message != None or '':
         if '^' in message:
-            prog = ('pyautogui.hotkey("' + '", "'.join(str(v) for v in message.split('^')) + '")')
-            exec(prog)
+            pyautogui.hotkey(*message.split('^'))
         else:
             pyautogui.write(message, interval = 0.2)
             pyautogui.press('enter')
