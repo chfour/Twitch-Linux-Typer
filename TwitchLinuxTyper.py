6416a6bdd4b5284dea89a73e2f122400976a15ff
chfour <michaleklitwin@gmail.com>
2022-01-26 19:29:19 +0100

    implement chat providers

--- TwitchLinuxTyper.py
+++ TwitchLinuxTyper.py
@@ -1,15 +1,17 @@
-from TwitchLinuxListener import Chat
-import pyautogui
+from chat_providers.basic import Chat
+import pyautogui, asyncio
 
 # The most cursed Twitch chat bot ever. do NOT run this on anything you care about
 
-MyChat = Chat()
+async def main(chat_provider_class: Chat):
+    chat = chat_provider_class()
+    while True:
+        message = await chat.get_message()
+        if message != None or '':
+            if '^' in message:
+                pyautogui.hotkey(*message.split('^'))
+            else:
+                pyautogui.write(message, interval = 0.2)
+                pyautogui.press('enter')
 
-while True:
-    user, message = MyChat.listen()
-    if message != None or '':
-        if '^' in message:
-            pyautogui.hotkey(*message.split('^'))
-        else:
-            pyautogui.write(message, interval = 0.2)
-            pyautogui.press('enter')
+asyncio.run(main(Chat))
